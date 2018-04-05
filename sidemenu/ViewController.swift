d16b909b5f224ecebb3f76818b991542e48a4b8e
khomsovon <sovon.khom68@gmail.com>
2018-04-05 13:33:57 +0700

    implement sidemenu

--- sidemenu/ViewController.swift
+++ sidemenu/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var menu: UIBarButtonItem!
+    @IBOutlet weak var alert: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
